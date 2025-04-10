bb4213ead832cd59223b6ab7490ff1e8904dd3a1
Mehakwadhwa <104835769@student.swin.edu.au>
2025-04-10 12:58:32 +1000

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,2 +1,18 @@
+def bfs(nodes, edges, origin, destinations):
+    frontier = deque([(origin, [origin])])
+    explored = set()
+    nodes_created = 1
 
-c
+    while frontier:
+        current, path = frontier.popleft()
+        if current in destinations:
+            return current, nodes_created, path
+        
+        explored.add(current)
+
+        for neighbor, _ in sorted(edges.get(current, [])):
+            if neighbor not in explored and all(neighbor != n for n, _ in frontier):
+                frontier.append((neighbor, path + [neighbor]))
+                nodes_created += 1
+    
+    return None, nodes_created, []
