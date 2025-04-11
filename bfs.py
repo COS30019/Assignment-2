d3f5a74346760c280de6a31f3040af036f5905b1
Mehakwadhwa <104835769@student.swin.edu.au>
2025-04-11 22:55:09 +1000

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,21 +1,26 @@
-import sys
 from collections import deque
 
 def bfs(nodes, edges, origin, destinations):
-    frontier = deque([(origin, [origin])])
-    explored = set()
-    nodes_created = 1
+    frontier = deque()
+    frontier.append((origin, [origin]))
+    visited = set()
+    nodes_created = 0
 
     while frontier:
-        current, path = frontier.popleft()
-        if current in destinations:
-            return current, nodes_created, path
-        
-        explored.add(current)
+        current_node, path = frontier.popleft()
+        nodes_created += 1
 
-        for neighbor, _ in sorted(edges.get(current, [])):
-            if neighbor not in explored and all(neighbor != n for n, _ in frontier):
-                frontier.append((neighbor, path + [neighbor]))
-                nodes_created += 1
-    
+        if current_node in destinations:
+            return current_node, nodes_created, path
+
+        if current_node not in visited:
+            visited.add(current_node)
+
+            neighbors = [(end, cost) for (start, end), cost in edges.items() if start == current_node]
+            neighbors.sort()
+
+            for neighbor, _ in neighbors:
+                if neighbor not in visited:
+                    frontier.append((neighbor, path + [neighbor]))
+
     return None, nodes_created, []
