0d3f35c9db52c9f20fe8bb975cbed69bbc110cf5
Arjun-0702 <arjunashok0702@gmail.com>
2025-04-11 21:26:13 +1000

    Update CUS1.py

--- CUS1.py
+++ CUS1.py
@@ -1,1 +1,23 @@
-def dls
+def dls (node,goal,edges,path,explored,limit,count):
+    if node in goal:
+        return node, count, path
+    
+    if limit <=0:
+        return None, count,[]
+    
+    explored.add(node)
+
+    for (_from, _to), _ in sorted(edges.items()):
+        if _from == node and _to not in explored:
+            new_path = path+[_to]
+            goal_found, created, found_path= dls(_to, goal, edges, new_path,explored, limit -1, count +1)
+            if goal_found:
+                return goal_found, created, found_path
+    
+    explored.remove(node)
+    return None,count,[]
+
+def cus1(nodes, edges,orgin, destination):
+    limit=10
+    explored=set()
+    return dls(orgin,set(destination), edges, [orgin], explored, limit, 1)
