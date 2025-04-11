3a55767f2a3bbbab1cfe5b6aafc9601db3f52ef9
BlairSlenders <68044009+BlairSlenders@users.noreply.github.com>
2025-04-11 18:19:48 +1000

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -2,17 +2,17 @@ import sys
 from parse_file import populate_graph, find_adjacent
 
 def dfs(nodes, edges, origin, destinations):
-    visited = set()
+    nodes_visited = set()
     stack = [(origin, [origin], 0)]
     nodes_created = 1
 
     while stack:
         node, path, cost = stack.pop()
 
-        if node in visited:
+        if node in nodes_visited:
             continue
         
-        visited.add(node)
+        nodes_visited.add(node)
 
         if node in destinations:
             return node, nodes_created, path
@@ -20,7 +20,7 @@ def dfs(nodes, edges, origin, destinations):
         adjacent = find_adjacent(node, edges)
 
         for adjacent, edge_cost in reversed(adjacent):
-            if adjacent not in visited:
+            if adjacent not in nodes_visited:
                 stack.append((adjacent, path + [adjacent], cost + edge_cost))
                 nodes_created = nodes_created + 1
                 
