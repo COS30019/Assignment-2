2fce45c37bcadcb3a38dad85646968685ac7ad3b
Arjun-0702 <arjunashok0702@gmail.com>
2025-04-11 20:06:24 +1000

    Update gbfs.py

--- gbfs.py
+++ gbfs.py
@@ -1,2 +1,29 @@
 import heapq
-from parse_file
+from parse_file import compute_heuristic
+
+def gbfs(nodes, edges, origin, destinations):
+    frontier = []
+    explored = set()
+    created = 0
+
+    heapq.heappush(frontier, (compute_heuristic(nodes, origin, destinations), origin, [origin]))
+    created += 1
+
+    while frontier:
+        h, node, route = heapq.heappop(frontier)
+        if node in destinations:
+            return node, created, route
+
+        if node in explored:
+            continue
+
+        explored.add(node)
+
+        for edge, cost in edges.items():
+            src, dst = edge
+            if src == node and dst not in explored:
+                updated_route = route + [dst]
+                heapq.heappush(frontier, (compute_heuristic(nodes, dst, destinations), dst, updated_route))
+                created += 1
+
+    return None, created, []
