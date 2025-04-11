48eda4b2008246f00774cb1e8aee7bd6da494070
BlairSlenders <68044009+BlairSlenders@users.noreply.github.com>
2025-04-11 21:10:38 +1000

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,21 +1,25 @@
 import heapq
-from parse_file import find_adjacent, compute_heuristic
+from parse_file import compute_heuristic, find_adjacent
 
-def astar(nodes, edges, origin, goals):
-    frontier = [(compute_heuristic(nodes, origin, goals), origin, [origin], 0)]
+def astar(nodes, edges, origin, destinations):
+    frontier = []
+    heapq.heappush(frontier, (0 + compute_heuristic(nodes, origin, destinations), origin, [origin], 0))
     visited = set()
     created = 1
+    
     while frontier:
-        f, node, path, g = heapq.heappop(frontier)
-        if node in goals:
-            return node, created, path
-        visited.add(node)
-
-        adjacent = find_adjacent(node, edges)
-        for adjacent_node, cost in adjacent:
+        f, current, path, g = heapq.heappop(frontier)
+        if current in destinations:
+             return current, created, path
+        
+        if current in visited:
+             continue
+         visited.add(current)
+        
+        for adjacent_node, cost in find_adjacent(current, edges):
             if adjacent_node not in visited:
                 g_new = g + cost
-                f_new = g_new + compute_heuristic(nodes, adjacent_node, goals)
+                f_new = g_new + compute_heuristic(nodes, adjacent_node, destinations)
                 heapq.heappush(frontier, (f_new, adjacent_node, path + [adjacent_node], g_new))
                 created += 1
     return None, created, []
