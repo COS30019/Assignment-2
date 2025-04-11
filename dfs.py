7ffc7369a4f8551acd9d681ffaf144618a2a4785
BlairSlenders <68044009+BlairSlenders@users.noreply.github.com>
2025-04-11 18:14:13 +1000

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,27 @@
+import sys
+from parse_file import populate_graph, find_adjacent
+
+def dfs(nodes, edges, origin, destinations):
+    visited = set()
+    stack = [(origin, [origin], 0)]
+    nodes_created = 1
+
+    while stack:
+        node, path, cost = stack.pop()
+
+        if node in visited:
+            continue
+        
+        visited.add(node)
+
+        if node in destinations:
+            return node, nodes_created, path
+        
+        adjacent = find_adjacent(node, edges)
+
+        for adjacent, edge_cost in reversed(adjacent):
+            if adjacent not in visited:
+                stack.append((adjacent, path + [adjacent], cost + edge_cost))
+                nodes_created = nodes_created + 1
+                
+    return None, nodes_created, []
