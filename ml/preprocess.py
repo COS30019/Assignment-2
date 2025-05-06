a9df20f811efcf50fc60d66e0bae70b41eb83757
Arjun-0702 <arjunashok0702@gmail.com>
2025-05-07 07:42:12 +1000

    Update preprocess.py

--- ml/preprocess.py
+++ ml/preprocess.py
@@ -1,2 +1,11 @@
-def load_data():
-# TODO: load and preprocess SCATS data
+def preview_csv(filepath):
+    df = pd.read_csv(filepath)
+    print(df.head())
+
+# sample csv file print
+#   site_id         timestamp  flow  latitude  longitude
+# 0      101  2025-04-01 00:00    35  -37.8136   144.9631
+# 1      101  2025-04-01 00:15    42  -37.8136   144.9631
+# 2      101  2025-04-01 00:30    38  -37.8136   144.9631
+# 3      101  2025-04-01 00:45    30  -37.8136   144.9631
+# 4      101  2025-04-01 01:00    45  -37.8136   144.9631
